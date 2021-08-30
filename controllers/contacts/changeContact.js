a2d384a3e92ca449cb115a57fbc00715b29dd454
deniskodak <denis.kodak@gmail.com>
2021-08-30 19:00:33 +0300

    refactoring changeContact function, add req.body validation

--- controllers/contacts/changeContact.js
+++ controllers/contacts/changeContact.js
@@ -1,18 +1,23 @@
 const { Contact } = require("../../model");
 
 const changeContact = async (req, res, next) => {
+  const bodyLength = Object.keys(req.body).length;
   try {
+    if (bodyLength < 1) {
+      return res.status(400).json({ message: "missing field favorite" });
+    }
+
     const contact = await Contact.findByIdAndUpdate(
       { _id: req.params.contactId },
       req.body,
       { new: true }
     );
 
-    if (contact) {
-      return res.status(200).json({ contact: { contact } });
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
     }
 
-    return res.status(404).json({ message: "Not found" });
+    return res.status(200).json({ contact: { contact } });
   } catch (error) {
     next(error);
   }
